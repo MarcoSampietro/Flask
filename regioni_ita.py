f87aae0c232933b3f50024cd03f3331607c61a3b
Marco Sampietro <sampietro.marco@iisgalvanimi.edu.it>
2024-04-22 08:31:47 +0000

    Aggiunti info

--- regioni_ita.py
+++ regioni_ita.py
@@ -1,13 +1,22 @@
+#scrivere un sito web che visualizza i nomi delle regioni italiane prese da un file csv memorizzato all'indirizzo 
+#https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/it')
-def regioni():
+def homepage():
     import pandas as pd
-    df = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
-    lista1 = sorted(list(set(regioni['denominazione_regione'])))    
-    return render_template("indexregioni_ita.html", lista = lista1)
+    regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+    risultato = sorted(list(set(regioni_italiane['denominazione_regione'])))
+    return render_template('indexregioni_ita.html', lista = risultato)
+
+@app.route('/info/<regione>', methods = ['GET'])
+def info(regione):
+  import pandas as pd
+  regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+  
+  return(regione)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
