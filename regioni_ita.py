7d1f905e941c7f082aac457ac411cab281270cc6
Marco Sampietro <sampietro.marco@iisgalvanimi.edu.it>
2024-04-22 08:59:10 +0000

    aggiunti interazioni con le regioni

--- regioni_ita.py
+++ regioni_ita.py
@@ -1,5 +1,3 @@
-#scrivere un sito web che visualizza i nomi delle regioni italiane prese da un file csv memorizzato all'indirizzo 
-#https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -15,8 +13,15 @@ def homepage():
 def info(regione):
   import pandas as pd
   regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
-  
-  return(regione)
+  info = regioni_italiane[regioni_italiane['denominazione_regione'] == regione]
+  return render_template('table.html', tabella = info.to_html())
+
+@app.route('/info_json/<regione>', methods = ['GET'])
+def info_json(regione):
+  import pandas as pd
+  regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+  info = regioni_italiane[regioni_italiane['denominazione_regione'] == regione]
+  return render_template('json.html', tabella = info.to_json())
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
